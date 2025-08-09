07205345007972e9add219f9d1b1b12bd1317324
hanynil <hany.mozaffary@gmail.com>
2025-08-09 08:47:27 +0330

    btn style

--- components/global/icon/warning.vue
+++ components/global/icon/warning.vue
@@ -1,8 +1,8 @@
 <template>
     <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M23 22.0415V31.6248" stroke="#913175" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M23 14.3942L23.0192 14.3729" stroke="#913175" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M23 42.1668C33.5855 42.1668 42.1667 33.5856 42.1667 23.0002C42.1667 12.4147 33.5855 3.8335 23 3.8335C12.4146 3.8335 3.83337 12.4147 3.83337 23.0002C3.83337 33.5856 12.4146 42.1668 23 42.1668Z" stroke="#913175" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M23 22.0415V31.6248" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M23 14.3942L23.0192 14.3729" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M23 42.1668C33.5855 42.1668 42.1667 33.5856 42.1667 23.0002C42.1667 12.4147 33.5855 3.8335 23 3.8335C12.4146 3.8335 3.83337 12.4147 3.83337 23.0002C3.83337 33.5856 12.4146 42.1668 23 42.1668Z" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 
 </template>
